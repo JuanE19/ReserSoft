51628f1cee872e3b9ff88321762ba68adb84af7b
JuanE19 <jesantana15@misena.edu.co>
2022-07-27 09:52:23 -0500

    algunas migraciones

---
+++ database/migrations/2022_07_27_141137_create__reservas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_reservas', function (Blueprint $table) {
+            $table->id();
+            $table->String("estado");
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_reservas');
+    }
+}
