8590566eb25f667f8479d9a2d19a72cd3b2e6d97
JuanE19 <jesantana15@misena.edu.co>
2022-08-12 07:29:00 -0500

    datatables

--- resources/views/reserva/edit.blade.php
+++ resources/views/reserva/edit.blade.php
@@ -10,7 +10,7 @@
 <form action="/reserva/{{$reserva->id}}" method="POST">
     @csrf
     @method('PUT')
-    <div class="container m-4 w-25">
+    <div class="container m-4 w-50">
     <div class="mb-3">
         <label for="" class="form-label">Nombre</label>
         <input id="nombre" name="nombre" type="text" class="form-control" tabindex="1" value="{{$reserva->nombre}}">
@@ -37,16 +37,22 @@
     </div>
 
     <a href="/reserva" class="btn btn-secondary" tabindex="5">Cancelar</a>
-    <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
+    <button type="submit" class="btn btn-success" tabindex="4">Guardar</button>
     </div>
 
 </form>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/admin_custom.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 @stop
--- resources/views/reserva/index.blade.php
+++ resources/views/reserva/index.blade.php
@@ -7,35 +7,34 @@
 @stop
 
 @section('content')
+<div class="container">
+    <form action="/reserva" method="POST">
+        @csrf
+        <div class="input-group mb-3">
+            <input id="nombre" name="nombre" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
+        </div>
 
-<!-- <a href="reserva/create" class="btn btn-success">Crear reserva</a> -->
+        <div class="input-group mb-3">
+            <input id="apellido" name="apellido" class="form-control" placeholder="apellido" aria-label="apellido" aria-describedby="basic-addon2">
 
-<form action="/reserva" method="POST">
-    @csrf
-    <div class="input-group mb-3">
-        <input id="nombre" name="nombre" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
-    </div>
+        </div>
 
-    <div class="input-group mb-3">
-        <input id="apellido" name="apellido" class="form-control" placeholder="apellido" aria-label="apellido" aria-describedby="basic-addon2">
+        <label for="basic-url" class="form-label">Fechas</label>
 
-    </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Llegada</span>
+            <input id="fechaDeIngreso" name="fechaDeIngreso" type="date" class="form-control mx-2">
+            <span class="input-group-text">Salida</span>
+            <input id="fechaDeSalida" name="fechaDeSalida" type="date" class="form-control mx-2">
+            <input id="cantidadDePersonas" name="cantidadDePersonas" type="text" class="form-control mx-2" placeholder="Cantidad de personas" aria-label="Cantidad de personas" aria-describedby="basic-addon2">
+            <button type="submit" class="btn btn-success" tabindex="4">Guardar</button>
+        </div>
 
-    <label for="basic-url" class="form-label">Fechas</label>
-
-    <div class="input-group mb-3">
-        <span class="input-group-text">Llegada</span>
-        <input id="fechaDeIngreso" name="fechaDeIngreso" type="date" class="form-control mx-2">
-        <span class="input-group-text">Salida</span>
-        <input id="fechaDeSalida" name="fechaDeSalida" type="date" class="form-control mx-2">
-        <input id="cantidadDePersonas" name="cantidadDePersonas" type="text" class="form-control mx-2" placeholder="Cantidad de personas" aria-label="Cantidad de personas" aria-describedby="basic-addon2">
-        <button type="submit" class="btn btn-success" tabindex="4">Guardar</button>
-    </div>
-
-</form>
+    </form>
+</div>
 
 <div class="container w-80">
-    <table class="table table-striped table-bordered shadow-lg mt-4">
+    <table id="dataTableReserva" class="table table-striped table-bordered shadow-lg mt-4">
         <thead class="bg-green text-white">
             <tr>
                 <th scope="col">Id</th>
@@ -58,9 +57,7 @@
                 <td>{{ $reserva->fechaDeSalida}}</td>
                 <td>
                     <form action="{{ route  ('reserva.destroy', $reserva->id)}}" method="POST">
-                        <a href="/reserva/{{ $reserva->id}}/edit" class="btn btn-info fas fa-edit"></a>
-
-                        @csrf
+                        <a href="/reserva/{{ $reserva->id}}/edit" class="bi bi-pencil-square">Editar</a>
                 </td>
             </tr>
             @endforeach
@@ -71,13 +68,27 @@
 @stop
 
 @section('css')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <link rel="stylesheet" href="/css/admin_custom.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 @stop
 
 @section('js')
-<script src="https://code.jsquery.com/jquery-3.2.1.js"></script>
-<script type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 <script>
-    console.log('Hi!');
+    $(document).ready(function() {
+        $('#dataTableReserva').DataTable({
+            language: {
+                "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
+            }
+        });
+    });
 </script>
 @stop
