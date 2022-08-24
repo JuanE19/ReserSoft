a02413c613945baae491cb6111d03c43395254a5
DavidMolina18 <davidmolinaurrego2@gmail.com>
2022-08-24 10:19:23 -0500

    perfil

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -73,6 +73,7 @@
                             <div class="modal-content">
                                 <div class="modal-header bg-green text-white">
                                     <h5 class="modal-title" id="exampleModalLabel"><strong>Información del Cliente</strong></h5>
+                                  
 
                                 </div>
                                 <div class="modal-body">
@@ -84,6 +85,7 @@
                                     <strong>Correo:</strong> {{ $cliente->Correo }} <br>
                                     <strong>Teléfono:</strong> {{ $cliente->Telefono }} <br>
                                     <strong>Dirección:</strong> {{ $cliente->Direccion }} <br>
+                                    
                                     
 
                                 </div>
@@ -164,10 +166,13 @@
 @stop
 
 @section('css')
+
 <link rel="stylesheet" href="/css/admin_custom.css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
 
 @stop
 
@@ -177,6 +182,7 @@
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 
 <script>
     $(document).ready(function() {
